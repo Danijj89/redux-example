a80cfd6d83aca9943d4cc1ba3055848af7f1ffbe
Daniele Zhan <dzhan1@wayfair.com>
2022-12-13 11:05:44 -0500

    adds fetch status example to redux

--- src/redux-example/duck/selectors.ts
+++ src/redux-example/duck/selectors.ts
@@ -1,4 +1,6 @@
 import { RootState } from "../../app/store";
+import { CommentStateStatus } from "./commentsSlice";
 
 export const selectComments = (state: RootState) => state.comments.comments;
 export const selectCommentsCount = (state: RootState) => state.comments.comments.length;
+export const selectCommentsStatus = (state: RootState): CommentStateStatus => state.comments.status;
