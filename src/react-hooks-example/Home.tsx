02eb76374c1edc9405e0ec29cd97cc71b0333987
Daniele Zhan <dzhan1@wayfair.com>
2022-12-13 11:13:59 -0500

    adds fetch status to context example

--- src/react-hooks-example/Home.tsx
+++ src/react-hooks-example/Home.tsx
@@ -1,30 +1,56 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { CommentCount } from './CommentCount';
-import { CommentsContext } from './context/CommentsContextProvider';
-import { getComments } from '../services';
-import { Comment } from '../types';
+import React, { useContext, useEffect, useState } from "react";
+import { CommentCount } from "./CommentCount";
+import { CommentsContext } from "./context/CommentsContextProvider";
+import { getComments } from "../services";
+import { Comment } from "../types";
 
 export const Home = () => {
-    const { comments, dispatchSetComments, dispatchAddComment } = useContext(CommentsContext);
-    const [text, setText] = useState('');
+  const { comments, dispatchSetComments, dispatchAddComment } =
+    useContext(CommentsContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [text, setText] = useState("");
 
-    useEffect(() => {
-        const fetchComments = async () => {
+
+
+  useEffect(() => {
+    const fetchComments = async () => {
+        try {
             const fetchedComments: Comment[] = await getComments();
             dispatchSetComments(fetchedComments);
+        } catch (e: any) {
+            setIsError(true);
         }
-        fetchComments();
-    }, [dispatchSetComments]);
+        setIsLoading(false);
+      };
 
-    return (
+    setIsLoading(true);
+    fetchComments();
+  }, [dispatchSetComments, setIsLoading]);
+
+  return (
+    <>
+      {isLoading && "Loading..."}
+      {!isLoading && (
         <>
-            <h2>All Comments</h2>
-            <input type="text" onChange={(e) => setText(e.target.value)} value={text} />
-            <button onClick={() => dispatchAddComment({ body: text })}>Add</button>
-            <ul>
-                {comments.map(comment => <li key={comment.body}>{comment.body}</li>)}
-            </ul>
-            <CommentCount />
+          <h2>All Comments</h2>
+          <input
+            type="text"
+            onChange={(e) => setText(e.target.value)}
+            value={text}
+          />
+          <button onClick={() => dispatchAddComment({ body: text })}>
+            Add
+          </button>
+          <ul>
+            {comments.map((comment) => (
+              <li key={comment.body}>{comment.body}</li>
+            ))}
+          </ul>
+          <CommentCount />
         </>
-    );
+      )}
+      {isError && "ERROR"}
+    </>
+  );
 };
